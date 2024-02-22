33e62913052ab47951f5722978a953e1f6824753
KangwonPark <nambaggangwon@gmail.com>
2024-02-22 17:31:21 +0900

    Variable Complete

---
+++ src/variable/Var8.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var8 {
+	public static void main(String[] args) {
+		byte b = 127; //-128~ 127
+		short s = 32767; //-32,768 ~ 32,767
+		int i = 2147483647; //약 -21 ~ 21(약 20억)
+
+		//몇 경정도
+		long l = 1234561234471231288L;
+
+		//실수
+		float f = 10.0f;
+		double d =10.0;
+
+	}
+}
---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,18 @@
+package variable.ex;
+
+public class VarEx3 {
+	public static void main(String[] args) {
+//		long a;
+//		a = 10000000000L;
+//		System.out.println(a);
+//		boolean b;
+//		b= true;
+//		System.out.println(b);
+
+		long longVar = 10000000000L;
+		System.out.println(longVar);
+		boolean booleanVar = true;
+		System.out.println(booleanVar);
+
+	}
+}
